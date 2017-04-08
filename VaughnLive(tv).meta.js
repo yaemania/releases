5bceca9606d399e6c9250bc216e4dedbbe560d35
yaemania <yaelmania@hotmail.com>
2017-04-08 14:05:40 -0600

    Add files via upload

--- VaughnLive(tv).meta.js
+++ VaughnLive(tv).meta.js
@@ -3,6 +3,8 @@
 // @namespace   Adios Facebook, pueblicidad, ads, etc.
 // @run-at      document-end
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js
+// @updateURL	https://raw.githubusercontent.com/yaemania/releases/master/VaughnLive(tv).meta.js
+// @downloadURL	https://raw.githubusercontent.com/yaemania/releases/master/VaughnLive(tv).user.js
 // @include     *://vaughnlive.tv*
 // @exclude     *://vaughnlive.tv/*chat*
 // @version     1.4
