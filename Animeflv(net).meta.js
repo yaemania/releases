ebbe33d211342b6ddd8ce781ca7878860ac6ff50
yaemania <yaelmania@hotmail.com>
2017-04-13 21:26:14 -0600

    Add files via upload

---
+++ Animeflv(net).meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        Animeflv.net - Fix
+// @namespace   Animeflv.net, Remove ads,social,and any shit i dont like :D
+// @description remove ads social, etc, just pure player style.
+// @run-at      document-end
+// @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js
+// @updateURL   https://raw.githubusercontent.com/yaemania/releases/master/Animeflv(net).meta.js
+// @downloadURL https://raw.githubusercontent.com/yaemania/releases/master/Animeflv(net).user.js
+// @include     http*://animeflv.net/*
+// @version     1.4
+// @dateLast    2017.04.13
+// @grant       unsafeWindow
+// @grant       GM_addStyle
+// ==/UserScript==
