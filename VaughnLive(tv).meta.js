b1790414b2e0ebd7fc31a90603846f8964bd4809
yaemania <yaelmania@hotmail.com>
2018-12-08 18:47:19 +0000

    Update VaughnLive(tv).meta.js

--- VaughnLive(tv).meta.js
+++ VaughnLive(tv).meta.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name        VaughnLive.tv
-// @namespace   Clean VaughnLive to see only player, no more crap else, also add-on for me web page http://yaelmania.co.nf
+// @namespace   Clean VaughnLive to see only player, no more crap else, also add-on from me web page http://yaelmania.co.nf
 // @run-at      document-end
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js
-// @updateURL	https://raw.githubusercontent.com/yaemania/releases/master/VaughnLive(tv).meta.js
+// @updateURL 	https://raw.githubusercontent.com/yaemania/releases/master/VaughnLive(tv).meta.js
 // @downloadURL	https://raw.githubusercontent.com/yaemania/releases/master/VaughnLive(tv).user.js
-// @include     *://vaughnlive.tv*
-// @exclude     *://vaughnlive.tv/*chat*
-// @version     1.7
-// @dateLast	2017.09.23
+// @include     *://vaughn*
+// @exclude     *://vaughn*/*chat*
+// @version     2.0
+// @dateLast  	2018.12.08
 // @grant       none
 // ==/UserScript==
