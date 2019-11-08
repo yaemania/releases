5f6d0c052439fe5cf3a40675a9bbe13023e97212
Yaelmania Ilutions Graphics <yaelmania@hotmail.com>
2019-11-08 02:31:05 +0000

    Update HDFull.meta.js

--- HDFull.meta.js
+++ HDFull.meta.js
@@ -2,8 +2,8 @@
 // @name        HDfull
 // @namespace   Clean HDfull, no more crap else.
 // @copyright   2017+, Yaelmania
-// @version     2.1.0
-// @dateLast    2018.10.28
+// @version     2.3.0
+// @dateLast    2019.11.07
 // @license     BSD
 // @homepageURL https://github.com/yaemania/releases
 // @run-at      document-end
@@ -11,19 +11,5 @@
 // @updateURL	https://raw.githubusercontent.com/yaemania/releases/master/HDFull.meta.js
 // @downloadURL	https://raw.githubusercontent.com/yaemania/releases/master/HDFull.user.js
 // @include     *://hdfull.*/*
-// @grant       unsafeWindow
-// @grant       GM_deleteValue
-// @grant       GM_getResourceURL
-// @grant       GM_getValue
-// @grant       GM_openInTab
-// @grant       GM_registerMenuCommand
-// @grant       GM_setValue
-// @grant       GM_xmlhttpRequest
-// @grant       GM.deleteValue
-// @grant       GM.getResourceUrl
-// @grant       GM.getValue
-// @grant       GM.openInTab
-// @grant       GM.setValue
-// @grant       GM.xmlHttpRequest
 // @grant       unsafeWindow
 // ==/UserScript==
