78f660a15d0eb2de9691baa585b206205248806f
Yaelmania Ilutions Graphics <yaelmania@hotmail.com>
2019-11-17 03:49:18 +0000

    Create new file

---
+++ NudeCollect_Fetch.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        NudeCollect_Fetch
+// @namespace   NudeCollect Fetch Gallery
+// @description Fetch gallery's up to 6 pages on group, then select next to get others 6 
+// @copyright   2019+, Yaelmania Ilutions Graphics (Yaelmania)
+// @run-at      document-end
+// @downloadURL https://github.com/yaemania/releases/raw/master/NudeCollect_Fetch.user.js
+// @updateURL   https://github.com/yaemania/releases/raw/master/NudeCollect_Fetch.meta.js
+// @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js
+// @include     http*://*nudecollect.com/content/*/index-mirror*.html
+// @include     http*://*nudecollect.com/content/*/page*pics*mirror*
+// @version     2.8.4
+// @grant       unsafeWindow
+// @grant       GM_addStyle
+// @grant       GM_xmlhttpRequest
+// ==/UserScript==
